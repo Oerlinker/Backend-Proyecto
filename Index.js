d7ad067c92b88f657ceab542a7f736b12ff16dd9
Oerlinker <oerlinker@gmail.com>
2024-10-16 20:55:15 -0400

    initial commit

--- Index.js
+++ Index.js
@@ -9,26 +9,23 @@ const logActivity = require('./middleware/logUserActivity');
 const bitacoraRoutes = require('./routes/bitacoraRoutes');
 
 const app = express();
-const port = 47210;
+const port = process.env.PORT || 3000;
 
 // Middlewares
 app.use(cors());
 app.use(express.json());
 
-//rutas bitacora
+// Middleware to log the URL
+
+
+// Rutas
 app.use('/api', logActivity('viewed libros'), libroRoutes);
 app.use('/api', logActivity('viewed autores'), autorRoutes);
 app.use('/api', logActivity('viewed usuarios'), userRoutes);
 app.use('/api', logActivity('viewed editoriales'), editorialRoutes);
 app.use('/api', logActivity('viewed categorias'), categoriaRoutes);
-
-// Rutas
-app.use('/api', libroRoutes);
-app.use('/api',autorRoutes);
-app.use('/api', userRoutes);
-app.use('/api', editorialRoutes);
-app.use('/api', categoriaRoutes);
 app.use('/api', bitacoraRoutes);
+
 // Ruta de bienvenida
 app.get('/', (req, res) => {
     res.send('juany ta gozu!');
