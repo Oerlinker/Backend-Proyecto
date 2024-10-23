29e885ceab4e3c49a777b8ba00d75ce8e4ead884
Oerlinker <oerlinker@gmail.com>
2024-10-23 13:00:01 -0400

    correcion middleware

--- middleware/verificarRol.js
+++ middleware/verificarRol.js
@@ -1,6 +1,6 @@
 const verificarRol = (rolesPermitidos) => {
     return (req, res, next) => {
-        const userRoleId = req.user.role;
+        const userRoleId = req.user.rol;
 
         if (!rolesPermitidos.includes(userRoleId)) {
             return res.status(403).json({ mensaje: 'Acceso denegado' });
