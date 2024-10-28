96ffa75e1575d17addbeceb2b1f714bee63a4e8f
Oerlinker <oerlinker@gmail.com>
2024-10-27 22:50:48 -0400

    error 404

--- models/userActivityLogModel.js
+++ models/userActivityLogModel.js
@@ -1,9 +1,9 @@
 const pool = require('../db');
-const logUserActivity = async (userId, action) => {
+const logUserActivity = async (userId, action,userIp) => {
     try {
         await pool.query(
-            'INSERT INTO useractivitylog (userid, action, timestamp) VALUES ($1, $2, NOW())',
-            [userId, action]
+            'INSERT INTO useractivitylog (userid, action, timestamp) VALUES ($1, $2, NOW(), $3)',
+            [userId, action,userIp]
         );
     } catch (error) {
         console.error('Error logging user activity', error);
