7bbf8e1b1f1d000018187553aae7048949398aac
Oerlinker <oerlinker@gmail.com>
2024-10-28 22:33:51 -0400

    proveedor fix

--- routes/proveedorRoutes.js
+++ routes/proveedorRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { addProveedor, getProveedor, getProveedorById, updProveedor, delProveedor } = require('../controllers/proveedorController');
+const { addProveedor, getProveedor, updProveedor, delProveedor, getProveedorByIdController} = require('../controllers/proveedorController');
 
 router.post('/proveedores', addProveedor);
 router.get('/proveedores', getProveedor);
-router.get('/proveedores/:id', getProveedorById);
+router.get('/proveedores/:id', getProveedorByIdController);
 router.put('/proveedores/:id', updProveedor);
 router.delete('/proveedores/:id', delProveedor);
 
