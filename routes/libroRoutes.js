a45856217d99077a398984d48d7d648ac9fa0741
Oerlinker <oerlinker@gmail.com>
2024-10-31 04:24:35 -0400

    fix

--- routes/libroRoutes.js
+++ routes/libroRoutes.js
@@ -18,7 +18,7 @@ router.get('/libros/:id', libroController.getLibroById);
 router.get('/search', searchLibros);
 router.post('/libros', adLibro);
 
-router.get('api/libros/:id/reseñas', libroController.getReseñas);
+router.get('api/review/libro:id', libroController.getReseñas);
 
 router.put('/libros/:id',updLibro);
 router.delete('/libros/:id', delLibro);
