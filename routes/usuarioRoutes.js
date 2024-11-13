808687f80f09e774ae8d0289763c7cf17d527ea9
Oerlinker <oerlinker@gmail.com>
2024-11-12 20:28:18 -0400

    puse vverificar

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -17,9 +17,9 @@ router.put('/users/name/:id',verificarTokenYRol([1,2,3,4]), updateUserName);
 router.put('/users/email/:id',verificarTokenYRol([1,2,3,4]), updateUserCorreo);
 router.put('/users/password/:id',verificarTokenYRol([1,2,3,4]), updateUserPassword);
 router.put('/users/update',verificarTokenYRol([4]), updateUserRoles);
-router.get('/users/prestamos/activos/:miembroid',vverificarTokenYRol([42,3,4]), prestamosActivos);
+router.get('/users/prestamos/activos/:miembroid',verificarTokenYRol([4,3,4]), prestamosActivos);
 router.post('/users/prestamos/devolver/:prestamoid',verificarTokenYRol([2,3,4]), prestamosDevolver);
-router.post('/users/review',vverificarTokenYRol([2,3,4]), hacerReseña);
+router.post('/users/review',verificarTokenYRol([2,3,4]), hacerReseña);
 router.get('/users/members/',verificarTokenYRol([3,4]), getMember);
 
 
