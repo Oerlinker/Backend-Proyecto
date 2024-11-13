9f1bb189d300dd4d59c8e9d2945b58ef9d1165fe
Oerlinker <oerlinker@gmail.com>
2024-11-13 00:43:56 -0400

    me dio miedo esto

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -9,7 +9,7 @@ const verificarTokenYRol = require("../middleware/verificarTokenYRol");
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.post('/create-subscription', createSubscriptionAndMember);
-
+router.get('/users/prestamos/activos/:miembroid', prestamosActivos);
 
 //protected routes
 router.get('/users',verificarTokenYRol([4]), getUser);
@@ -17,7 +17,6 @@ router.put('/users/name/:id',verificarTokenYRol([1,2,3,4]), updateUserName);
 router.put('/users/email/:id',verificarTokenYRol([1,2,3,4]), updateUserCorreo);
 router.put('/users/password/:id',verificarTokenYRol([1,2,3,4]), updateUserPassword);
 router.put('/users/update',verificarTokenYRol([4]), updateUserRoles);
-router.get('/users/prestamos/activos/:miembroid',verificarTokenYRol([2,3,4]), prestamosActivos);
 router.post('/users/prestamos/devolver/:prestamoid',verificarTokenYRol([2,3,4]), prestamosDevolver);
 router.post('/users/review',verificarTokenYRol([2,3,4]), hacerReseña);
 router.get('/users/members/',verificarTokenYRol([3,4]), getMember);
