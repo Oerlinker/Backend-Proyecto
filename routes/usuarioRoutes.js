2b6bdca1fb57df1cd6536916d859fa52adfbf69c
Oerlinker <oerlinker@gmail.com>
2024-11-13 00:55:00 -0400

    consulta prestamo

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -17,6 +17,7 @@ router.put('/users/name/:id',verificarTokenYRol([1,2,3,4]), updateUserName);
 router.put('/users/email/:id',verificarTokenYRol([1,2,3,4]), updateUserCorreo);
 router.put('/users/password/:id',verificarTokenYRol([1,2,3,4]), updateUserPassword);
 router.put('/users/update',verificarTokenYRol([4]), updateUserRoles);
+router.get('/users/prestamos/activos/:miembroid',verificarTokenYRol([2,3,4]), prestamosActivos);
 router.post('/users/prestamos/devolver/:prestamoid',verificarTokenYRol([2,3,4]), prestamosDevolver);
 router.post('/users/review',verificarTokenYRol([2,3,4]), hacerReseña);
 router.get('/users/members/',verificarTokenYRol([3,4]), getMember);
