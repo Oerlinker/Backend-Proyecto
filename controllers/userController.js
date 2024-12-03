ac6ef263d04779d1f5c6372b4f485d92403e2110
Oerlinker <oerlinker@gmail.com>
2024-12-03 05:25:11 -0400

    correo 31

--- controllers/userController.js
+++ controllers/userController.js
@@ -422,6 +422,7 @@ const updateMemberSemestreByID = async (req, res) => {
 const forgotPassword = async (req, res) => {
     const { email } = req.body;
     if (!email) {
+        console.log('Email is missing in the request body');
         return res.status(400).json({ message: 'Email is required' });
     }
 
