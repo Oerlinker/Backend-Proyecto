6d9fc0bfc8e598e22bd522b47579b62fff48a418
Oerlinker <oerlinker@gmail.com>
2024-12-11 16:38:21 -0400

    CU Andres 2

--- routes/validRegistroRoutes.js
+++ routes/validRegistroRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { createValidRegistroController } = require('../controllers/validRegistroController');
+const { createValidRegistroController,getValidRegistrosController } = require('../controllers/validRegistroController');
 const verificarTokenYRol = require("../middleware/verificarTokenYRol");
 
 router.post('/valid_registros',verificarTokenYRol[(4)], createValidRegistroController);
-router.get('/valid_registros',verificarTokenYRol[(4)], createValidRegistroController);
+router.get('/valid_registros',verificarTokenYRol[(4)], getValidRegistrosController);
 
 module.exports = router;
