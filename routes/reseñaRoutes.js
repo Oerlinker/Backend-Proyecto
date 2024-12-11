8a2bd133304653fc8e6032c8d14ab6d7d5f6caea
Oerlinker <oerlinker@gmail.com>
2024-12-11 15:34:16 -0400

    CU Ivan 3

--- routes/reseñaRoutes.js
+++ routes/reseñaRoutes.js
@@ -4,8 +4,8 @@ const verificarTokenYRol = require("../middleware/verificarTokenYRol");
 
 const router = express.Router();
 
-router.post('/reseñas/reportar', verificarTokenYRol([2, 3, 4]), reportarReseñaController);
-router.get('/reseñas/reportadas', verificarTokenYRol([3, 4]), obtenerReseñasReportadasController);
-router.delete('/reseñas/:reseñaid/:edicionid/:libroid', verificarTokenYRol([3, 4]), eliminarReseñaController);
+router.post('/reseñas/reportar', verificarTokenYRol([2]), reportarReseñaController);
+router.get('admin/reseñas/reportadas', verificarTokenYRol([3, 4]), obtenerReseñasReportadasController);
+router.delete('admin/reseñas/:reseñaid/:edicionid/:libroid', verificarTokenYRol([3, 4]), eliminarReseñaController);
 
 module.exports = router;
